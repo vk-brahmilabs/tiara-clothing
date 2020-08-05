d62a1fa01a66d4f726e6eae7ccc48fe8a2e38556
vaishnavikchari <vaishnavichari@gmail.com>
2020-08-05 14:59:03 +0530

    Added email signin

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
 
     this.state = {
       email: '',
@@ -17,19 +17,25 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+  handleSubmit = async event => {
+    event.preventDefault ();
+    const {email, password} = this.state;
 
-    this.setState({email: '', password: ''});
+    try {
+      await auth.signInWithEmailAndPassword (email, password);
+      this.setState ({email: '', password: ''});
+    } catch (error) {
+      console.log (error);
+    }
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     const {value, name} = event.target;
 
-    this.setState({[name]: value});
+    this.setState ({[name]: value});
   };
 
-  render() {
+  render () {
     return (
       <div className="sign-in">
         <h2>I already have an account</h2>
