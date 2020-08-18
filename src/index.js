71099cfade4380299fc2644e7c7f9a0545d0e2e4
vaishnavikchari <vaishnavichari@gmail.com>
2020-08-18 18:24:36 +0530

    added service worker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './redux/store';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render (
   <Provider store={store}>
@@ -18,3 +19,5 @@ ReactDOM.render (
   </Provider>,
   document.getElementById ('root')
 );
+
+serviceWorker.register ();
